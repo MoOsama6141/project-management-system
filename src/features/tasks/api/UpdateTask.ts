3a9a5fbeca904c2748f52c9569b7cb54c0bcecb9
mostafa-osama <front-007@almusanid.com>
2026-09-10 15:38:19 +0300

    feat: restrict edit and delete options to managers in project table

--- src/features/tasks/api/UpdateTask.ts
+++ src/features/tasks/api/UpdateTask.ts
@@ -7,7 +7,7 @@ const updateTask = async (
     title: string;
     description: string;
     employeeId: string;
-    projectId: string;
+    id: string;
     status: string;
   },
 ) => {
@@ -15,7 +15,7 @@ const updateTask = async (
 
   try {
     const response = await axios.put(
-      `${API_V1}/Task/${taskId}/change-status`,
+      `${API_V1}/Task/${taskId}`,
       {
         ...payload,
         employeeId: payload.employeeId,
